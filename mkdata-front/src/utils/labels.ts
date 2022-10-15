d02dad304fb878184f4db1fb4b9679b87938e202
fernandosenacruz <fernandosenacruz@gmail.com>
2022-10-15 16:32:27 -0300

    refactor: :recycle: Input de identificação
    Remove input

--- mkdata-front/src/utils/labels.ts
+++ mkdata-front/src/utils/labels.ts
@@ -2,12 +2,10 @@ export interface ILabels {
   types: string[];
   groups: string[];
   status: string[];
-  identifications: string[];
 }
 
 const labels: ILabels = {
   types: ['CPF', 'CNPJ'],
-  identifications: ['RG', 'IE'],
   status: ['Ativo', 'Invativo'],
   groups: ['SAÚDE', 'EMPRESARIAL', 'CONCULTORIA'],
 };
