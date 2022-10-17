8dc0064066620d5cc4bf5c7d17e65840cf852d08
fernandosenacruz <fernandosenacruz@gmail.com>
2022-10-17 13:43:48 -0300

    .

--- mkdata-back/src/index.ts
+++ mkdata-back/src/index.ts
@@ -5,6 +5,6 @@ process.on('SIGTERM', () => {
   process.exit();
 });
 
-const PORT = process.env.APP_PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 App.listen(PORT, () => console.log('App listening at %d', PORT));
